ac9007e341ef90cc530e4ab5fe4e1fdf253abb73
svyhlidka <44548975+svyhlidka@users.noreply.github.com>
2018-11-29 10:34:46 +0100

    first step

--- 3D-SupportsVolume/_3D_SupportsVolume.py
+++ 3D-SupportsVolume/_3D_SupportsVolume.py
@@ -1,1 +1,8 @@
+from PyQt5.QtWidgets import * #QWidget, QApplication, QFrame, QMessageBox, QLabel, QDesktopWidget,  QMainWindow, QDialog
+import sys, random
+from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import *
+import winsound         # for sound  
+import time             # for sleep
+app = QApplication(sys.argv)
 
