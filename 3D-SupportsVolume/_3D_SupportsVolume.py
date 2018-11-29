9d52c508b653367ee2e0f93af75573ec577a16bc
svyhlidka <44548975+svyhlidka@users.noreply.github.com>
2018-11-29 11:26:33 +0100

    reader

--- 3D-SupportsVolume/_3D_SupportsVolume.py
+++ 3D-SupportsVolume/_3D_SupportsVolume.py
@@ -4,5 +4,18 @@ from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import *
 import winsound         # for sound  
 import time             # for sleep
-app = QApplication(sys.argv)
+#app = QApplication(sys.argv)
 
+
+class MyReader():
+
+    def load_file(self,path):
+        self.file = open(path)
+        i = 0
+        for line in self.file:
+            print(line)
+            i += 1
+            if i > 20: break
+
+x = MyReader()
+x.load_file(r"C:/3D/Petr/Cube_for_support_ASCII.stl")
