3e433430d7eb86a9f4a56f11517004aa93fb7089
svyhlidka <44548975+svyhlidka@users.noreply.github.com>
2018-11-29 14:00:21 +0100

    dictionary with max z added

--- 3D-SupportsVolume/_3D_SupportsVolume.py
+++ 3D-SupportsVolume/_3D_SupportsVolume.py
@@ -8,14 +8,21 @@ import time             # for sleep
 
 
 class MyReader():
-
+    
+   
     def load_file(self,path):
-        self.file = open(path)
-        i = 0
-        for line in self.file:
-            print(line)
-            i += 1
-            if i > 20: break
+        lines = [line.replace("vertex","").strip().split(' ') for line in open(path) if "vertex" in line]
+        lines1 = []
+        lines1 = [[float(line[0]), float(line[1]), float(line[2])] for line in lines]
+        for i in range(10):
+            print(lines1[i])
+        dict = {}
+        for item in lines1:
+            if not (item[0],item[1]) in dict: dict.update({(item[0],item[1]):item[2]})
+            if (item[0],item[1]) in dict and item[2] > dict[(item[0],item[1])]: dict[(item[0],item[1])]=item[2]
+        #dict1 = {{(item[0],item[1]):item[2]} for item in lines1}
+        #dict1={{(item[0],item[1]):item[2]} if not (item[0],item[1]) in dict else dict[(item[0],item[1])]=item[2] for item in lines1 }
+        print(len(lines1),len(dict)) #,len(dict1))
 
 x = MyReader()
 x.load_file(r"C:/3D/Petr/Cube_for_support_ASCII.stl")
